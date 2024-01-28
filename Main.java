78ae59dfc8f5580dc9b07d0c2c06517c2409b165
Bekir Tamrak <56741929+tamrakk@users.noreply.github.com>
2024-01-28 19:35:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,58 @@
+package Not_OrtalamaHesap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Kullanıcıdan veri alma
+        Scanner input = new Scanner(System.in);
+
+        //Değişkenler
+        int matematik,fizik,kimya, turkce,tarih,muzık,toplam;
+        double ortalama;
+        boolean kosul1;
+        String sonuc;
+
+
+        System.out.println("Not Ortlama Hesaplama");
+
+        //Değer alama
+
+        System.out.print("Matematik Notunu Giriniz:");
+        matematik = input.nextInt();
+
+        System.out.print("Fizik Notunu Giriniz:");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Notunu Giriniz:");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe Notunu Giriniz:");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih Notunu Giriniz:");
+        tarih = input.nextInt();
+
+        System.out.print("Müzik Notunu Giriniz:");
+        muzık = input.nextInt();
+
+        toplam = matematik + fizik + kimya + turkce + tarih + muzık;
+
+        //ortalama hesaplama
+          ortalama = toplam/6;
+        System.out.println("Not Ortalamanız: "+ ortalama);
+
+        // koşul Durumu
+        kosul1 = ortalama >= 60;
+        sonuc=(kosul1) ? "Sınıfı Geçti":"Sınıfta Kaldı";
+        System.out.println(sonuc);
+
+
+
+
+
+
+
+
+    }
+}
